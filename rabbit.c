12490953936c627f800967323fd36236e851242a
Seno Hirokazu <hirokazukisaragi@gmail.com>
2017-12-26 20:46:33 +0900

    remove unuse printf,and add comment

--- rabbit.c
+++ rabbit.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
+/*********************
+first argment is target filename
+secont arg is crypt keyword
+third arg is select decrypt or encrypt
+ **********************/
 int main(int argc,char* argv[]){
   FILE *rfp,*wfp;
   if(argc != 4){
@@ -17,33 +21,42 @@ int main(int argc,char* argv[]){
   char key[512];
   strcpy(key,argv[2]);
   char wname[1024];
-  strcpy(wname,argv[1]);
-  strcat(wname,"_c.crpt");
+  if(!strcmp(argv[3],"e")){
+    strcpy(wname,argv[1]);
+    strcat(wname,"_c.crpt");
+  }
+  if(!strcmp(argv[3],"d")){
+    strcpy(wname,argv[1]);
+    strcat(wname,".origin");
+  }
   if(!(wfp = fopen(wname,"w+"))){
     printf("write error!\n");
   }
   int i=0;
   int j=0;
-  int len = strlen(key);
   if(!strcmp(argv[3],"e")){
+    //cryption code
     while(!feof(rfp)){
-      printf("cryption!!\n");
       fread(buf,sizeof(char),1,rfp);
       if(key[i])i=0;
       buf[j] = buf[j]+key[i];
       if(feof(rfp))break;
       fwrite(buf,sizeof(char),1,wfp);
     }
+    //finish
+    printf("cryption!!\n");
   }
   if(!strcmp(argv[3],"d")){
+    //decrypt!
     while(!feof(rfp)){
-      printf("decryption!!\n");
       fread(buf,sizeof(char),1,rfp);
       if(key[i])i=0;
       if(feof(rfp))break;
       buf[j] = buf[j]-key[i];
       fwrite(buf,sizeof(char),1,wfp);
     }
+    //end
+    printf("decryption!!\n");
   }
   
   fclose(rfp);
